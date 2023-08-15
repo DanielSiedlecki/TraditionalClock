eca572899d0c77d927ab0a8246475a425d028947
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-08-15 23:08:55 +0200

    Add reload-listener, functions to move the clock hands

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const reload = document.getElementById("button-reset");
+
+reload.addEventListener("click", () => {
+  window.location.reload();
+});
 timezone.addEventListener("change", handleTimezoneChange);
 
 function handleTimezoneChange() {
@@ -53,26 +58,52 @@ async function fetchCountryList(timezone) {
 function setTime(datetime) {
   const time = datetime.substring(11, 19);
   const timeArray = time.split(":");
+  setFirstTime(timeArray[0], timeArray[1]);
 
   setInterval(() => {
     if (timeArray[2] < 60) {
       timeArray[2]++;
-      console.log(timeArray[2]);
     } else {
       timeArray[2] = 0;
       if (timeArray[1] < 59) {
         timeArray[1]++;
+        LinesMoveMinutes(timeArray[1]);
       } else {
         timeArray[1] = 0;
         if (timeArray[0] < 23) {
           timeArray[0]++;
+          LinesMoveHour(timeArray[0]);
         } else {
           timeArray[0] = 0;
         }
       }
-      console.log("add Minute");
     }
   }, 1000);
+}
+
+function setFirstTime(hour, minutes) {
+  const line_minutes = document.getElementById("line-minutes");
+
+  line_minutes.style.transform = `translateY(-45%) rotate(${minutes * 6}deg)`;
+  line_minutes.style.transition = " 0.9s ease-in-out";
+
+  const line_hour = document.getElementById("line-hours");
+
+  line_hour.style.transform = `translateY(-45%) rotate(${hour * 15}deg)`;
+  line_hour.style.transition = " 0.9s ease-in-out";
+}
+
+function LinesMoveMinutes(minutes) {
+  const line_minutes = document.getElementById("line-minutes");
+
+  line_minutes.style.transform = `translateY(-45%) rotate(${minutes * 6}deg)`;
+  line_minutes.style.transition = " 0.3s ease-in-out";
+}
+function LinesMoveHour(hour) {
+  const line_hour = document.getElementById("line-hours");
+
+  line_hour.style.transform = `translateY(-45%) rotate(${hour * 15}deg)`;
+  line_hour.style.transition = " 0.3s ease-in-out";
 }
 
 fetchTimezoneList();
